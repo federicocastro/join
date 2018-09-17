58b4fc8417425ffabed7d1c3bc8843922268215b
Federico Castro <federico.castro@comprandoengrupo.net>
2018-09-16 22:13:32 -0300

    prod settings

--- join/join/settings/prod.py
+++ join/join/settings/prod.py
@@ -15,9 +15,9 @@ INSTALLED_APPS += [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME', 'join'),
-        'USER': os.environ.get('DATABASE_USER', 'join'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'join'),
+        'NAME': os.environ.get('DATABASE_NAME', 'beluz'),
+        'USER': os.environ.get('DATABASE_USER', 'beluz'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'beluz'),
         'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DATABASE_PORT', '5432'),
         'OPTIONS': {

17e3a90c2222aa546a6731b3cbfd2e12d64dd9d7
Federico Castro <federico.castro@comprandoengrupo.net>
2018-10-05 10:34:37 -0300

    Adding celery beat

--- join/join/settings/base.py
+++ join/join/settings/base.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
+    'django_celery_beat',
     'photologue',
     'sortedm2m',
     'channels',
